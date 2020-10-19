15edc5ee08fbbc9b2dea4c7f4e9aeb956c5a0c89
young sik seo <angelskying@gmail.com>
2020-10-19 15:43:00 +0900

    웹팩 설정 추가 진행함

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { HotModuleReplacementPlugin } = require('webpack')
+const path = require('path');
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
   return {
@@ -10,17 +13,34 @@ module.exports = (env, argv) => {
       filename: 'bundle.js'
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
         filename: '[name].bundle.css',
         chunkFilename: '[id].css'
       }),
       new HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({}),
+      new HtmlWebpackPlugin({
+        hash: true, // 정적 파일을 불러올때 쿼리문자열에 웹팩 해쉬값을 추가한다
+        template: './src/index.html', // 템플릿 경로를 지정
+        // templateParameters: { // 템플릿에 주입할 파라매터 변수 지정
+        //   env: process.env.NODE_ENV === 'develop' ? '(개발용)' : '',
+        // },
+        minify: process.env.NODE_ENV === 'production' ? {
+          collapseWhitespace: true, // 빈칸 제거
+          removeComments: true, // 주석 제거
+        } : false,
+      }),
+      
     ],
     devServer: {
       open: true,
+      overlay: true,
       clientLogLevel: 'silent',
-      contentBase: './dist',
+      contentBase: path.join(__dirname, "dist"),
       historyApiFallback: true,
+      port: 8000,
+      stats: "errors-only",
       hot: true,
     },
     module: {
